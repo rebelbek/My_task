fedc656fcba0c1a4094cf20e4eeb8c059885df7d
bek <rebelbek@mail.ru>
2025-01-01 16:32:07 +0300

    Незначительные изменения, для удобства

--- task1.py
+++ task1.py
@@ -1,9 +1,10 @@
 import os
 import requests
 from dotenv import dotenv_values, load_dotenv
-from bs4 import BeautifulSoup #внешний модуль
-#Дополнительно должен быть установлен модуль lxml(он нужен для BeautifulSoup)
+from bs4 import BeautifulSoup  # внешний модуль
+# Дополнительно должен быть установлен модуль lxml(он нужен для BeautifulSoup)
 
+# используется каталог .env для скрытия url
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
@@ -11,24 +12,22 @@ moex_api_url = os.environ.get('url')
 
 response = requests.get(moex_api_url)
 soup = BeautifulSoup(response.content, 'xml')
+
+# для выборки полей из данных xml
 list_names = ['secid', 'boardid', 'shortname', 'lotsize', 'secname', 'listlevel', 'issuesize']
+data = None
+
+for i in soup.find_all('data'):
+    if i['id'] == 'securities':
+        data = i
+        break
+rows = data.select('row')
 
 
-def get_rows():
-    data = None
-    for i in soup.find_all('data'):
-        if i['id'] == 'securities':
-            data = i
-            break
-    rows = data.select('row')
-    return rows
-
-
-def get_stocks_list():
-    rows = get_rows()
+def get_stocks_list() -> list:
     stocks_list = []
     for row in rows:
-        tmp_dict = {} #для временного хранения значений из одной строки row
+        tmp_dict = {}  # для временного хранения значений из одной строки row
         for item in list_names:
             if row[item.upper()].isdigit():
                 tmp_dict[item] = int(row[item.upper()])
@@ -38,7 +37,7 @@ def get_stocks_list():
     return stocks_list
 
 
-def show_stocks_list():
+def show_stocks_list() -> None:
     stocks_list = get_stocks_list()
     for item in stocks_list:
         tmp_list = []
@@ -47,7 +46,6 @@ def show_stocks_list():
         print(' | '.join(tmp_list))
         print('-' * 130)
 
+#Если открывается из самого скрипта без импортирования
 if __name__ == "__main__":
     show_stocks_list()
-
-
